5139ab1d70345a71bb829d3f97865bfee35b8ab5
田口 <chienduc@mor.com.vn>
2024-03-24 17:48:11 +0800

    update

--- resources/views/frontend/includes/nav.blade.php
+++ resources/views/frontend/includes/nav.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
-    <a href="{{ route('frontend.index') }}" class="navbar-brand">{{ app_name() }}</a>
+    <a href="{{ route('frontend.auth.login') }}" class="navbar-brand">{{ app_name() }}</a>
 
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="@lang('labels.general.toggle_navigation')">
         <span class="navbar-toggler-icon"></span>
@@ -7,14 +7,6 @@
 
     <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
         <ul class="navbar-nav">
-            @if(config('locale.status') && count(config('locale.languages')) > 1)
-                <li class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownLanguageLink" data-toggle="dropdown"
-                       aria-haspopup="true" aria-expanded="false">@lang('menus.language-picker.language') ({{ strtoupper(app()->getLocale()) }})</a>
-
-                    @include('includes.partials.lang')
-                </li>
-            @endif
 
             @auth
                 <li class="nav-item"><a href="{{route('frontend.user.dashboard')}}" class="nav-link {{ active_class(Route::is('frontend.user.dashboard')) }}">@lang('navs.frontend.dashboard')</a></li>
@@ -41,8 +33,6 @@
                     </div>
                 </li>
             @endguest
-
-            <li class="nav-item"><a href="{{route('frontend.contact')}}" class="nav-link {{ active_class(Route::is('frontend.contact')) }}">@lang('navs.frontend.contact')</a></li>
         </ul>
     </div>
 </nav>
--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -3,25 +3,25 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @if($link)
+  @if(isset($link))
     <title>{{ $link->title }}</title>
     <meta name="description" content="{{ $link->description }}">
     <meta property="og:title" content="{{ $link->keywords }}" />
     <meta property="og:description" content="{{ $link->description }}" />
     <meta property="og:image" content="{{ asset('storage/img/link/'.$link->thumbnail_image) }}" />
     <meta property="og:url" content="{{ $link->original_link }}" />
-  @else
+  @elseif(isset($setting))
     <title>Other</title>
     <meta property="og:url" content="{{ $setting->default_redirect_link }}" />
   @endif
 </head>
 <body>
   <script>
-    @if($link)
+    @if(isset($link))
       setTimeout(function() {
         window.location.href = "{{ $link->original_link }}";
       }, 50);
-    @else
+    @elseif(isset($setting))
       setTimeout(function() {
         window.location.href = "{{ $setting->auto_redirect_to }}";
       }, 50);
